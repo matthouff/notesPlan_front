438fabc67515a38f8ca30afeb5b60f9e7d0ebc43
MBerthelot <m.berthelot@asrec-cvl.org>
2023-09-26 09:23:14 +0200

    Ajout du contenu de la page notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,30 @@
+import { Button, Typography } from "@mui/material";
+import DefaultBox from "../components/DefaultBox";
+import { Link } from "react-router-dom";
+
 function NotFound() {
   return (
-    "404"
-  )
+    <DefaultBox
+      persoStyle={{
+        alignItems: "center",
+        justifyContent: "center",
+        px: 5,
+        pt: 0,
+        gap: 5,
+        color: "#fff",
+      }}
+    >
+      <Typography variant="h1" textAlign="center">
+        Erreur 404
+      </Typography>
+      <Typography variant="body1" fontWeight="bold" textAlign="center">
+        La page que vous recherchez n&apos;existe pas.
+      </Typography>
+      <Button component={Link} variant="contained" to="/">
+        Retour à la page d&apos;accueil
+      </Button>
+    </DefaultBox>
+  );
 }
 
 export default NotFound;
