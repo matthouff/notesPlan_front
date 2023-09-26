57fd2a1f1fe9ec5ad3b0403b61f6612a17ce37a9
MBerthelot <m.berthelot@asrec-cvl.org>
2023-09-26 09:24:11 +0200

    correction des redirections lors de la connexion

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,43 @@
-import './App.css'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import HomePage from './pages/home'
-import Notes from './pages/notes'
-import NotFound from './pages/NotFound'
-import Taches from './pages/taches'
-import Connexion from './pages/connexion'
-import { ThemeProvider } from '@mui/material'
-import Theme from './utils/style'
-import useAuth from './hooks/useAuth'
+import "./App.css";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home";
+import Notes from "./pages/notes";
+import NotFound from "./pages/NotFound";
+import Taches from "./pages/taches";
+import Connexion from "./pages/connexion";
+import { ThemeProvider } from "@mui/material";
+import Theme from "./utils/style";
+import useAuth from "./hooks/useAuth";
 
 function App() {
-
-  const { isAuthenticated } = useAuth()
+  const { isAuthenticated } = useAuth();
 
   return (
     <ThemeProvider theme={Theme}>
-      <BrowserRouter >
+      <BrowserRouter>
         <Routes>
-          <Route path="/auth" element={!isAuthenticated ? <Connexion /> : <HomePage />} />
-          <Route path="/" element={isAuthenticated ? <HomePage /> : <Navigate to="/auth" />} />
-          <Route path="/notes" element={isAuthenticated ? <Notes /> : <Navigate to="/" />} />
-          <Route path="/taches" element={isAuthenticated ? <Taches /> : <Navigate to="/" />} />
+          <Route
+            path="/"
+            element={isAuthenticated ? <HomePage /> : <Navigate to="/auth" />}
+          />
+          <Route
+            path="/notes"
+            element={isAuthenticated ? <Notes /> : <Navigate to="/auth" />}
+          />
+          <Route
+            path="/taches"
+            element={isAuthenticated ? <Taches /> : <Navigate to="/auth" />}
+          />
+
+          <Route
+            path="/auth"
+            element={!isAuthenticated ? <Connexion /> : <Navigate to="/" />}
+          />
           <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter >
+      </BrowserRouter>
     </ThemeProvider>
-  )
+  );
 }
 
-export default App
+export default App;
